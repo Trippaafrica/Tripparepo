1374e2861cf008f64dc3e8aa6fbb4e474f201bd9
trippaafrica <info@trippaafrica.com>
2025-05-17 21:39:03 +0100

    Hardcode the Google Maps API key to ensure it works

--- src/components/AddressAutocomplete.tsx
+++ src/components/AddressAutocomplete.tsx
@@ -31,7 +31,7 @@ const loadGoogleMapsScript = (callback: () => void) => {
   // Create script element
   const script = document.createElement('script');
   script.id = 'google-maps-script';
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDAPCO0RO432dTqJm0tH94o3g5s-kliK9o&libraries=places';
   script.async = true;
   script.defer = true;
   
